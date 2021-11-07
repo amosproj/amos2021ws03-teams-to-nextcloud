39464186024fc86052341aa166279f135388a1e8
ali <ali.aziritobi@gmail.com>
2021-11-07 10:05:31 +0100

    Drag and Drop Prototype

--- src/components/DragAndDrop.vue
+++ src/components/DragAndDrop.vue
@@ -1,5 +1,15 @@
 <template>
         <div style="border-style:solid;" @dragover.prevent @drop.prevent>
+      <input type="file" multiple @change="uploadFile"/>
+      <div @drop="dragFile" style="background-color:green;margin-bottom:10px;padding:10px;">
+        Or drag the file here
+        <div v-if="File.length">
+          <ul v-for="file in File" :key="file">
+            <li>{{file.name}}</li>
+          </ul>
+        </div>
+      </div>
+
     </div>
 </template>
 
