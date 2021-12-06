a7952895c526b70455629a63ce6eb66200042372
Smoothex <momchildp@gmail.com>
2021-12-06 12:20:00 +0100

    added progress bar for deletion

--- frontend/src/actions/Delete.js
+++ frontend/src/actions/Delete.js
@@ -1,7 +1,13 @@
 import Action from "./Action";
+import { app } from '@/main'
 import { store } from '@/store/index';
+import { useToast } from "vue-toastification";
 
 class Delete extends Action {
+    // Emiter for the events
+    emitter = null;
+
+    toast = null;
 
     constructor() {
         super("delete", "Delete", "M19 24h-14c-1.104 0-2-.896-2-2v-17h-1v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2h-1v17c0 1.104-.896 2-2 2zm0-19h-14v16.5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-16.5zm-9 4c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm-2-7h-4v1h4v-1z");
@@ -19,12 +25,34 @@ class Delete extends Action {
         this.enabled = enabled;
     }
 
-    async execute (pointerEvent) {
-        console.log(pointerEvent);
+    async execute () {
+        // Get toast interface for notifications
+        this.toast = useToast();
+        
         let client = store.getters.StateWebdavClient;
         let selected = store.getters.StateSelectedChildren;
+        let deletedFiles = 0;
+
+        this.emitter = app.config.globalProperties.emitter;
+            
+        // Show the progress bar when deleting
+        this.emitter.emit("SHOW_PROGRESS_BAR", "Deleting " + selected.length + " item" + (selected.length == 1 ? "" : "s") + "...");
+        
         for (let i = 0; i < selected.length; i++) {
-            await client.deleteFile(selected[i].path);
+            let currentFile = selected[i];
+            await client.deleteFile(currentFile.path);
+            deletedFiles++;
+
+            // Fill the progress bar
+            this.emitter.emit("PROGRESS_BAR_WIDTH", (deletedFiles / selected.length) * 100);
+            
+            // Hide the progress bar when all the selected files have been deleted
+            if (deletedFiles == selected.length) {
+                setTimeout(() => this.emitter.emit("HIDE_PROGRESS_BAR"), 1000);
+            }
+            
+            // Show a notification
+            this.toast.success("Deleted " + currentFile.name);
         }
         store.dispatch("loadChildrenForPath");
     }
