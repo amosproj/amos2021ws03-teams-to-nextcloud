23e8476fb81f5cf8ce4652b316539aa638cdca92
Smoothex <momchildp@gmail.com>
2021-12-07 21:49:28 +0100

    fixed bug (opening dir inside of a dir)

--- frontend/src/actions/OpenInNextcloud.js
+++ frontend/src/actions/OpenInNextcloud.js
@@ -22,17 +22,19 @@ class OpenInNextcloud extends Action {
     execute (pointerEvent) {
         console.log(pointerEvent);
         let selectedFiles = store.getters.StateSelectedChildren;
-        let currentDir = store.getters.StatePath[store.getters.StatePath.length-1].name;
+        let currentDirPath = store.getters.StatePath[store.getters.StatePath.length-1].path;
+        let currentDirPathSuffix = currentDirPath.replace(new RegExp('.*' + "com"), '');
         // One selected item
         if (selectedFiles.length == 1) {
+            let selectedFilesPathSuffix = selectedFiles[0].path.replace(new RegExp('.*' + "com"), '');
             if (selectedFiles[0].directory) {
-                window.open(process.env.VUE_APP_NEXTCLOUD_BASE_URL + "index.php/apps/files?dir=//" + selectedFiles[0].name, "_blank");
+                window.open(process.env.VUE_APP_NEXTCLOUD_BASE_URL + "index.php/apps/files?dir=//" + selectedFilesPathSuffix, "_blank");
             } else {
-                window.open(process.env.VUE_APP_NEXTCLOUD_BASE_URL + "index.php/apps/files?dir=//" + currentDir, "_blank");
+                window.open(process.env.VUE_APP_NEXTCLOUD_BASE_URL + "index.php/apps/files?dir=//" + currentDirPathSuffix, "_blank");
             }
         // No selected items 
         } else {
-            window.open(process.env.VUE_APP_NEXTCLOUD_BASE_URL + "index.php/apps/files?dir=//" + currentDir, "_blank");
+            window.open(process.env.VUE_APP_NEXTCLOUD_BASE_URL + "index.php/apps/files?dir=//" + currentDirPathSuffix, "_blank");
         }
     }
 }
