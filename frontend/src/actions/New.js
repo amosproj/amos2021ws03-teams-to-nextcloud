785b26014f80466afabb0628ff73cf1db062d082
zlatinametodieva <64097972+zlatinametodieva@users.noreply.github.com>
2022-01-12 20:46:47 +0100

    moved svg paths from actions into a separate file

--- frontend/src/actions/New.js
+++ frontend/src/actions/New.js
@@ -1,10 +1,11 @@
 import Action from "./Action";
 import { store } from '@/store/index';
+import { NavBarIcons } from "@/util/NavBarIcons";
 
 class New extends Action {
 
     constructor() {
-        super("new", "New", "M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z");
+        super("new", "New", NavBarIcons.CREATE);
     }
 
     setEnabled(enabled) {
