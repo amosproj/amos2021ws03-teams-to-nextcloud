f785591760703024da2587a75a8878d414f3d49e
zlatinametodieva <64097972+zlatinametodieva@users.noreply.github.com>
2022-01-19 21:14:33 +0100

    round checkboxes #64

--- frontend/src/components/FileList.vue
+++ frontend/src/components/FileList.vue
@@ -3,7 +3,7 @@
     <table class="table table-hover">
       <thead>
         <tr>
-          <th scope="col" style="width: 16px; text-left">
+          <th scope="col" style="width: 16px;">
             <div class="custom-control custom-checkbox">
               <input
                 :checked="allFilesSelected"
@@ -94,4 +94,53 @@ export default {
 
 <style>
   .fiv-cla, .fiv-viv, .fiv-sqo { font-size: 1.4em; }
+
+  @media (hover: hover) {
+    [type=checkbox] {
+      visibility: hidden;
+    }
+
+    tr:hover [type="checkbox"] {
+      visibility: visible;
+      background-color: white;
+    }
+
+    [type="checkbox"]:hover {
+      background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E %3Cpath d='M15.88 8.29L10 14.17l-1.88-1.88a.996.996 0 1 0-1.41 1.41l2.59 2.59c.39.39 1.02.39 1.41 0L17.3 9.7a.996.996 0 0 0 0-1.41c-.39-.39-1.03-.39-1.42 0z' /%3E %3C/svg%3E");
+    }
+  }
+
+  [type=checkbox] {
+    -webkit-appearance: none;
+    border-radius: 50%;
+    width: 17px;
+    height: 17px;
+    color: #6872b6;
+    background-size: contain;
+    outline: none;
+  }
+
+  [type=checkbox]::before {
+    content: "";
+    display: block;
+    height: inherit;
+    border-radius: inherit;
+    background-size: contain;
+    box-shadow: inset 0 0 0 1px #4b4b4b;
+  }
+
+  [type=checkbox]:checked {
+    visibility: visible;
+    background-color: currentcolor;
+    box-shadow: 0 0 6px #4b4b4b;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E %3Cpath d='M15.88 8.29L10 14.17l-1.88-1.88a.996.996 0 1 0-1.41 1.41l2.59 2.59c.39.39 1.02.39 1.41 0L17.3 9.7a.996.996 0 0 0 0-1.41c-.39-.39-1.03-.39-1.42 0z' fill='%23fff'/%3E %3C/svg%3E");
+  }
+
+  [type=checkbox]:checked::before {
+    box-shadow: none;
+  }
+
+  :hover [type="checkbox"]:checked {
+    background-color: #6872b6;
+  }
 </style>
