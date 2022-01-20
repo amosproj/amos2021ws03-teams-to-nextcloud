a6f1939e073fa326b39db0d0f6f9c34e4d7cde88
rbalink <robert.balink2@gmail.com>
2022-01-20 21:16:13 +0100

    copylink with modal

--- frontend/src/actions/CopyLink.js
+++ frontend/src/actions/CopyLink.js
@@ -1,8 +1,6 @@
 import Action from "./Action";
 import { store } from '@/store/index';
-import { parseXML } from 'webdav';
 import { NavBarIcons } from "@/util/NavBarIcons";
-import { useToast } from "vue-toastification";
 
 class CopyLink extends Action {
 
@@ -23,36 +21,6 @@ class CopyLink extends Action {
     }
 
     async execute () {
-        let client = store.getters.StateWebdavClient;
-        let path = store.getters.StatePath;
-        let directoryPath = path[path.length - 1].path;
-        let toast = useToast();
-        if (directoryPath == null) {
-            return;
-        }
-        let directoryClipboard = "";
-        // Checks if the selected object is in a subfolder or not
-        if (!directoryPath.endsWith("/")) {
-            // If selected object is in a subfolder, adjust path
-            directoryPath += "/";
-            // Extracts the name of the directory, also deletes last slash in string for clipboard link
-            directoryClipboard = directoryPath.split(path[0].path)[1].slice(0,-1);
-        }
-        // Builds path with directory path + fileName
-        let filePath = directoryPath + store.getters.StateSelectedChildren[0].name;
-        const response = await client.customRequest(filePath, { 
-            method: "PROPFIND",
-            data: '<?xml version="1.0" encoding="UTF-8"?><d:propfind xmlns:d="DAV:"><d:prop xmlns:oc="http://owncloud.org/ns"><oc:fileid/></d:prop> </d:propfind>'
-        });
-        // Splits the response after the first fileId tag and after the second one to receive the fileId
-        const xmlData = await parseXML(response.data);
-        let fileId = xmlData.multistatus.response[0].propstat.prop.fileid;
-        // Builds link for clipboard 
-        let clipboardLink = process.env.VUE_APP_NEXTCLOUD_BASE_URL + "index.php/apps/files?dir=/"+directoryClipboard+"&openfile="+fileId;
-        // Copy link to clipboard
-        toast.success("Link in clipboard: \n" + clipboardLink);
-        //window.prompt("Test", clipboardLink);
-        //await navigator.clipboard.writeText(clipboardLink);
         store.commit("setIsCopyLinkModalVisible", true);
     }
 }
